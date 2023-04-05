7feabb935bd710e6609e178d69ce7d23d3f012a1
andrehcarvalho6 <andre.hcarvalho1@gmail.com>
2023-04-05 14:43:12 +0100

    add a movie

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+
+import moviesFromJson from "./data/movies.json";
+
 import Header from "./components/Header";
-import Main from "./components/Main.js";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
 
 function App() {
+  const [moviesArr, setMoviesArr] = useState(moviesFromJson);
+
+  const deleteMovie = (movieTitle) => {
+    const newList = moviesArr.filter(
+      (movieDetails) => movieDetails.title !== movieTitle
+    );
+    setMoviesArr(newList);
+  };
+
+  const addMovie = (newMovie) => {
+    setMoviesArr((prevMoviesArr) => {
+      const newList = [newMovie, ...prevMoviesArr];
+      return newList;
+    });
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header numberOfMovies={moviesArr.length} />
+      <AddMovie callbackToAdd={addMovie} />
+      <Main moviesArr={moviesArr} callbackToDelete={deleteMovie} />
       <Footer />
     </div>
   );
